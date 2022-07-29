faf239e37f7817445e2c18b97f4f6b8b3f23014a
astgr <astgrey100500@gmail.com>
2022-07-29 13:38:57 +0300

    add project code

--- src/components/app/posts/postItem/PostItem.tsx
+++ src/components/app/posts/postItem/PostItem.tsx
@@ -10,12 +10,16 @@ const PostItem = ({ post }: IPostItemProps): JSX.Element => (
   <li data-testid={post.question_id}>
     <a href={post.link} data-testid={`post_link_${post.question_id}`}>
       <div className="item">
-        <img
-          className="image"
-          data-testid={`post_image_${post.question_id}`}
-          src={post.owner.profile_image}
-          alt={post.owner.display_name}
-        />
+        {
+          post.owner.profile_image && (
+          <img
+            className="image"
+            data-testid={`post_image_${post.question_id}`}
+            src={post.owner.profile_image}
+            alt={post.owner.display_name}
+          />
+          )
+        }
         <div className="title" data-testid={`post_title_${post.question_id}`}>{ post.title }</div>
       </div>
     </a>
